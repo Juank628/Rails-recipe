28d2a44a7e76caf308a233143d323f8d950943b6
PrangonGhose <prangonghose@gmail.com>
2023-04-18 21:39:34 +0600

    Create necessary models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
     belongs_to :user
-    has_many :food_recipes
-    has_many :recipes, through: :food_recipes
+    has_many :recipe_foods, dependent: :destroy
+    has_many :recipes, through: :recipe_foods
 end
