b788290de26bddcc4d34a21849f4ce694b79c39a
PrangonGhose <prangonghose@gmail.com>
2023-04-18 21:39:22 +0600

    Create migration files and schema for database

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_213238) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_151211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
-    t.float "price"
-    t.integer "quantity"
+    t.decimal "price"
+    t.decimal "quantity"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
-  create_table "foods_recipes", id: false, force: :cascade do |t|
+  create_table "recipe_foods", force: :cascade do |t|
+    t.decimal "quantity"
     t.bigint "recipe_id", null: false
     t.bigint "food_id", null: false
-    t.integer "quantity"
-    t.index ["food_id", "recipe_id"], name: "index_foods_recipes_on_food_id_and_recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.time "preparation_time"
-    t.time "cooking_time"
-    t.text "description"
+    t.string "preparation_time"
+    t.string "cooking_time"
+    t.string "description"
     t.boolean "public"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
@@ -51,5 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_213238) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
