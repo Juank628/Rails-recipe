59cfbc08bcffaff2937e4f8b2477f1513ed36701
PrangonGhose <prangonghose@gmail.com>
2023-04-19 20:53:02 +0600

    Update routes to have necessary controller access

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'public_recipes/index'
+  get 'public_recipes/show'
+  get 'recipes/index'
+  get 'recipes/show'
+  devise_for :users
+  root to: 'recipes#index'
+  resources :recipes, only: %i[index show new create]
+  resources :public_recipes, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
