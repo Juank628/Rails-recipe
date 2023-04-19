766e1079d5780a0bbfba5002771237e280a470f3
Juank628 <jsanchez_zunino@yahoo.com>
2023-04-19 14:05:27 -0500

    Create delete button in food index view

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,26 +1,30 @@
 class FoodsController < ApplicationController
-    def index
-        @foods = current_user.foods
-    end
+  def index
+    @foods = current_user.foods
+  end
 
-    def new
-        @food = Food.new
-    end
+  def new
+    @food = Food.new
+  end
 
-    def create
-        @food = current_user.foods.new(food_params)
-        @food.user = current_user
-    
-        respond_to do |format|
-          if @food.save
-            format.html { redirect_to foods_path }
-          else
-            format.html { render :new }
-          end
-        end
+  def create
+    @food = current_user.foods.new(food_params)
+    @food.user = current_user
+
+    respond_to do |format|
+      if @food.save
+        format.html { redirect_to foods_path }
+      else
+        format.html { render :new }
       end
-    
-      def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-      end
+    end
+  end
+
+  def destroy
+    p '******************************************************'
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
