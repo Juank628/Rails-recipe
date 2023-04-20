f7d610e9166e1ffcc236d63ab60deeebc2347b3e
Juank628 <jsanchez_zunino@yahoo.com>
2023-04-20 15:22:10 -0500

    Filter general shopping list by current user

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,10 +1,13 @@
 class ShoppingListsController < ApplicationController
   def index
     @ingredients = RecipeFood.group(:food).sum(:quantity)
-    @foods = Food.all
+    @ingredients = @ingredients.select { |ingredient| ingredient.user_id == current_user.id }
+    @foods = current_user.foods.all
     @shopping_list = []
     @to_buy = 0
     @total_value = 0
+
+    return if @ingredients.length == 0 || @foods.length == 0
 
     @ingredients.each do |ingredient|
       @to_buy = ingredient[1] - @foods.find(ingredient[0].id).quantity
