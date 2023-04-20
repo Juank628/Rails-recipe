cc5f06aae180d5d56f5a59ea4d328a16e096bf14
PrangonGhose <prangonghose@gmail.com>
2023-04-21 01:39:09 +0600

    Remove unnecessary routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'recipes#index'
-  resources :recipes, only: %i[index show new create update]
+  resources :recipes, only: %i[index show new create update destroy]
   resources :public_recipes, only: %i[index show]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :foods
-  resources :recipe_foods, only: %i[new create update]
+  resources :foods, only: %i[index show new create destroy]
+  resources :recipe_foods, only: %i[new create update destroy]
 end
