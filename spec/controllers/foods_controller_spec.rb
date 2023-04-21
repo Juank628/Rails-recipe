a2fdcb01d6211046273114ff9805b770a8ff49b7
Juank628 <jsanchez_zunino@yahoo.com>
2023-04-21 14:19:07 -0500

    Create foods controller tests

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -5,4 +5,44 @@ RSpec.describe FoodsController, type: :request do
         get '/foods'
         expect(response).to be_successful
     end
+
+    it 'ensures the response body has the correct text' do
+        get '/foods'
+        expect(response.body).to include('Foods')
+    end
+
+    it 'ensures the response body has Add Food button' do
+        get '/foods'
+        expect(response.body).to include('Add food')
+    end
+
+    it 'should response /foods/new' do
+        get '/foods/new'
+        expect(response).to be_successful
+    end
+
+    it 'ensures the response body has Add Food button' do
+        get '/foods/new'
+        expect(response.body).to include('Add food')
+    end
+
+    it 'ensures the response body has Name field' do
+        get '/foods/new'
+        expect(response.body).to include('Name')
+    end
+
+    it 'ensures the response body has Unit field' do
+        get '/foods/new'
+        expect(response.body).to include('Measurement unit')
+    end
+
+    it 'ensures the response body has Price field' do
+        get '/foods/new'
+        expect(response.body).to include('Price')
+    end
+
+    it 'ensures the response body has Quantity field' do
+        get '/foods/new'
+        expect(response.body).to include('Quantity')
+    end
 end
