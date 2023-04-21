75c152f2e57448d7e733932f75f3bd9c5dc69d6f
PrangonGhose <prangonghose@gmail.com>
2023-04-22 00:32:27 +0600

    Add validations for recipe_food model

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -3,4 +3,5 @@ class RecipeFood < ApplicationRecord
   belongs_to :recipe
 
   validates :food_id, uniqueness: { scope: :recipe_id }
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
