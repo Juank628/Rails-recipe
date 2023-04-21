aa5f0d71be599e4a6f7e8a572dba46d339b52a85
PrangonGhose <prangonghose@gmail.com>
2023-04-22 00:28:13 +0600

    Add tests for recipe model

---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  subject do
+    Recipe.new(
+      name: 'Recipe title',
+      preparation_time: 1,
+      cooking_time: 1,
+      description: 'description',
+      public: false
+    )
+  end
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'preparation time should be present' do
+    subject.preparation_time = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'preparation time should be positive' do
+    subject.preparation_time = -10
+    expect(subject).to_not be_valid
+  end
+
+  it 'cooking time should be present' do
+    subject.cooking_time = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it 'cooking time should be positive' do
+    subject.cooking_time = -10
+    expect(subject).to_not be_valid
+  end
+end
